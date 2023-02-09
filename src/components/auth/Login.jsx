64ae4000e5bd9437a18b211d62618100e05cc830
Joy He <joyhe520@gmail.com>
2023-02-08 18:55:05 -0600

    login page structure

---
+++ src/components/auth/Login.jsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react'
+import '../../css/styles.css';
+
+
+
+
+
+const Login = () => {
+ 
+  return (
+    <div className="login">
+   
+      <div className="picture-login">
+        <img className="toDoPicture" src="https://images.unsplash.com/photo-1598791318878-10e76d178023?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80" alt="To do list picture" />
+      </div>
+      
+      <div className="login-form">this is the login form section</div>
+    </div>
+        
+    
+  )
+}
+
+export default Login
